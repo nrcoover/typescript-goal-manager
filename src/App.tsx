2249f9a764ad81dcca4a28344d3850a8c6894610
nicklefritz <nrcoover@gmail.com>
2025-12-01 17:05:50 -0500

    Dynamically display list of goals

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
+import { useState } from "react";
+
 import goalsImg from "./assets/goals.jpg";
 import CourseGoal from "./components/CourseGoal";
 import Header from "./components/Header";
 
+type CourseGoal = {
+	id: number;
+	title: string;
+	description: string;
+};
+
 export default function App() {
+	const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+	const handleAddGoal = () => {
+		const newGoal: CourseGoal = {
+			id: Math.random(),
+			title: "Placeholder title",
+			description: "Placeholder description",
+		};
+
+		setGoals((previousGoals) => {
+			return [...previousGoals, newGoal];
+		});
+	};
+
 	return (
 		<main>
 			<Header image={{ src: goalsImg, alt: "A list of goals" }}>
 				<h1>You Course Goals</h1>
 			</Header>
-			<CourseGoal title="Learn React + TypeScript">
-				<p>Learn it from the ground up.</p>
-			</CourseGoal>
+			<button onClick={handleAddGoal}>Add Goal</button>
+			<ul>
+				{goals.map((goal) => {
+					return (
+						<li key={goal.id}>
+							<CourseGoal title={goal.title}>
+								<p>{goal.description}</p>
+							</CourseGoal>
+						</li>
+					);
+				})}
+			</ul>
 		</main>
 	);
 }
